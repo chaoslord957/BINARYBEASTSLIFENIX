5a750ad5ae56aa4e73ddc2027cd51b9e9c146ab6
2025CSB098 AHILAN_BHATTACHARYA <2025csb098.ahilan@students.iiests.ac.in>
2025-11-02 04:20:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2971415764-d40e9",
+  "appId": "1:976726156275:web:c4d1d67fb836a544636886",
+  "apiKey": "AIzaSyBfH2XtkaECrWOitUsIi7hdYz58pnXvmkM",
+  "authDomain": "studio-2971415764-d40e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "976726156275"
+};
